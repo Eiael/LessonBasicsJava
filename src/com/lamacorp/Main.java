ebfde06d1dcb145cd37f287ae62597d55f1c09b2
Eiael <youssef.eloutmani@gmail.com>
2022-03-19 13:49:43 +0100

    Initial commit

---
+++ src/com/lamacorp/Main.java
@@ -0,0 +1,11 @@
+package com.lamacorp;
+
+import com.lamacorp.teachpoo.lesson.ex1.Exercise1;
+import com.lamacorp.teachpoo.lesson.ex1.Lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Lesson1();
+    }
+}
